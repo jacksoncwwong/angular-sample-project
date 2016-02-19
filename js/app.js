3e7bd1e4d0ba4aeac1dd184120d79961275a9e33
JacksonWong <jackson.cw.wong@gmail.com>
2016-02-18 19:54:05 -0500

    talking about controller and view
    read the comments for some description

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-angular.module("myApp", [])
+angular.module("myApp", []);
+
+// now to define our controller
+// controllers take our data from the front-end(view)
+// and then operates on that data
+
+// In Angular, the view and the controller are bound using
+// an object called $scope
+
+var myController = function($scope){
+
+}
